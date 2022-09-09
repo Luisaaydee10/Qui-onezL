2118fc291673e8e07c06378572a6cc3c0c17ef0a
Luisaaydee10 <luisaquinonezavila@gmail.com>
2022-09-09 17:18:35 -0500

    T Version

--- suma.cpp
+++ suma.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-        float A,B,C;
-	cout<<"Ingrese 2 nuneros A y B :";
-        cin>>A>>B;
-        C=A+B;
+        float x,y,z;
+	cout<<"Ingrese 2 numeros separados de espacio x y :";
+        cin>>x>>y;
+        z=x+y;
 	cout<<"El resultado es :";
-        cout<<C;
+        cout<<z<<endl;
         return 0;
 }
